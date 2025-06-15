cf209f5b1681a3eedc3722d3e3919de14d1e49fe
Matthias Voigt <matthias@kinderderzeit.de>
2025-06-15 21:07:04 +0200

    improved

--- src/game/game.config.ts
+++ src/game/game.config.ts
@@ -26,7 +26,7 @@ export const gameConfig: Types.Core.GameConfig = {
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { x: 0, y: 0 },
+			gravity: { x: 0, y: 600 },
 			debug: false
 		}
 	},
